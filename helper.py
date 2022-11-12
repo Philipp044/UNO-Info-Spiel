3c227f389df77b39a97b683331e8d3685f793a1e
Philipp044 <118071182+Philipp044@users.noreply.github.com>
2022-11-12 18:21:00 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -5,4 +5,6 @@ def get_photo_image(path: str, new_size):
 
     return ImageTk.PhotoImage(Image.open(path).resize(new_size))
 
+#Mit dieser Funktion wird das Bild der Uno Karte erzeugt, sie wird aber erst wenn sie in Main.py aufgerufen wird wirksam
+
 
